6a69b4e6e06e613f05dfed413ff3ce484e5324df
Barış KIYIPINAR <102514336+Bariss1907@users.noreply.github.com>
2022-04-16 00:50:18 +0300

    Update bootstrapkutuphanesi.js

--- bootstrapkutuphanesi.js
+++ bootstrapkutuphanesi.js
@@ -1,1 +1,3 @@
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
